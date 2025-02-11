ad01ce82b8acd9d1fc7b41fee0b71d36410e356f
Sebastian <sebbe.nguyen@gmail.com>
2025-02-11 18:18:21 +0100

    Skapa lite klasser

--- clientModel.java
+++ clientModel.java
@@ -1,3 +1,26 @@
+import java.awt.*; 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle.Control;
+import java.io.*;
+import java.lang.reflect.Array;
+
+
+interface User {
+    public String userName = "";
+    public void changeName(String newName);
+    public String getUsername();
+}
+
+
+interface message {
+    public String message = "";
+    public String user = "";
+    public void sendMessage(String message);
+}
+
+
 public class clientModel {
     
 }
