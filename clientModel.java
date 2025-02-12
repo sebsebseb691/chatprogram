f2b9ac95fe2984e97430e76f350ecedd7f1472d4
Sebastian <sebbe.nguyen@gmail.com>
2025-02-12 15:19:41 +0100

    change username

--- clientModel.java
+++ clientModel.java
@@ -22,5 +22,25 @@ interface message {
 
 
 public class clientModel {
+
+    public String userName = "";
+    public String getUsername() {return userName;}
+
+    /**
+    * Change a username if it is not empty and does not contain blankspace
+    *
+    * @param newName new username as a string
+    * @throws RuntimeException if newName is empty or contains blankspace
+    */
+    public void changeName(String newName) throws RuntimeException {
+        if(newName.isEmpty()) throw new RuntimeException("Username cannot be empty");
+        else if (newName.contains(" ")) throw new RuntimeException("Username cannot contain blankspace");
+        else {
+            userName = newName;
+            //Update login page view
+        }
+    }
+
     
+
 }
