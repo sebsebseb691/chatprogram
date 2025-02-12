50abae0c12b7d55d8bd70d46696141d21e04a9f1
Sebastian <sebbe.nguyen@gmail.com>
2025-02-12 16:29:32 +0100

    loginpage

--- message.java
+++ message.java
@@ -1,3 +1,10 @@
-public class message {
-    
+public class Message {
+    User u;
+    private String msg;
+    private String user;
+
+    public Message(String msg) {
+        this.msg = msg;
+        user = u.getUsername();
+    }
 }
---
+++ views/view.java
@@ -0,0 +1,7 @@
+package views;
+
+import javax.swing.*;
+
+public interface view {
+
+}
