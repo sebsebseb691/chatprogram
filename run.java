a1a449f0d2787d46e06f698d96a10b9ce4e79c93
Sebastian <sebbe.nguyen@gmail.com>
2025-02-18 13:03:26 +0100

    rename files

--- run.java
+++ run.java
@@ -3,7 +3,7 @@ import models.*;
 import views.*;
 import server.*;
 
-public class Test {
+public class run {
 
     public static void main(String[] args){
         LoginPage_c c = new LoginPage_c();
