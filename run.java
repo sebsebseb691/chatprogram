4608771ff4bc273ec01806f8e26505e897a5f504
RichardEmmi <ricemm@chalmers.se>
2025-03-03 21:22:13 +0100

    Förbättrad version av Uml-Diagrammet

--- run.java
+++ run.java
@@ -4,7 +4,7 @@ import models.Client;
 public class run {
     public static void main(String[] args) {
         Client client = Client.getInstance();
-        client.connect("192.168.1.155", 54321);
+        client.connect("192.168.1.73", 54321);
 
         ControllersFacade cf = new ControllersFacade();
         cf.openLoginPage();
